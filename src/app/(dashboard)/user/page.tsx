9f4416ece2e2e5b576f22b87a696f03672a3fb4d
Menura Adithya <64089619+emiz98@users.noreply.github.com>
2024-06-17 23:52:09 +0530

    added user root page

---
+++ src/app/(dashboard)/user/page.tsx
@@ -0,0 +1,11 @@
+"use client";
+
+const UserPage = () => {
+  return (
+    <div className="bg-[#fafffc] h-screen p-5 overflow-y-scroll pb-24">
+      <div className="font-medium text-gray-400">Student Dashboard</div>
+    </div>
+  );
+};
+
+export default UserPage;
