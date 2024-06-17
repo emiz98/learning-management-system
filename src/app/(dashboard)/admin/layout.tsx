1913abb34f23df75c0adeb4758ab4a7871bf934a
Menura Adithya <64089619+emiz98@users.noreply.github.com>
2024-06-17 23:51:39 +0530

    added admin layout

---
+++ src/app/(dashboard)/admin/layout.tsx
@@ -0,0 +1,36 @@
+import Sidebar from "@/components/(dashboard)/sidebar/sidebar";
+import UserHeaderCard from "@/components/user_header_card/user_header_card";
+import { authOptions } from "@/lib/auth_options";
+import type { Metadata } from "next";
+import { getServerSession } from "next-auth";
+
+export const metadata: Metadata = {
+  title: "Academix Admin",
+  icons: "/favicon.png",
+};
+
+export default async function AdminLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  const session = await getServerSession(authOptions);
+
+  return (
+    <div className="grid grid-cols-12 h-screen overflow-hidden">
+      <Sidebar />
+      <div className="col-span-10 grid grid-cols-1">
+        <div className="h-20 col-span-1 w-full bg-white shadow-md border-b px-5 py-2 flex items-center justify-between">
+          <h3>Welcome {session?.user?.email?.split("@")[0]}! 👋</h3>
+          <div className="flex items-center gap-x-3">
+            <UserHeaderCard
+              username={session?.user?.email}
+              role={session?.user?.role}
+            />
+          </div>
+        </div>
+        <div className="col-span-1">{children}</div>
+      </div>
+    </div>
+  );
+}
