9b22edb512fc5316470cff2c876745685b159bec
Menura Adithya <64089619+emiz98@users.noreply.github.com>
2024-06-17 23:48:19 +0530

    added session context

---
+++ src/app/session_context.tsx
@@ -0,0 +1,18 @@
+"use client";
+
+import { SessionProvider } from "next-auth/react";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+
+export const queryClient = new QueryClient();
+
+export default function SessionContextProvider({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <SessionProvider>
+      <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
+    </SessionProvider>
+  );
+}
