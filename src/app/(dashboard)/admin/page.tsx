f75ad7f6292850ffdb1a270bd720b03304f0927c
Menura Adithya <64089619+emiz98@users.noreply.github.com>
2024-06-17 23:51:59 +0530

    added admin root page

---
+++ src/app/(dashboard)/admin/page.tsx
@@ -0,0 +1,11 @@
+"use client";
+
+const AdminPage = () => {
+  return (
+    <div className="bg-[#fafffc] h-screen p-5 overflow-y-scroll pb-24">
+      <div className="font-medium text-gray-400">Admin Dashboard</div>
+    </div>
+  );
+};
+
+export default AdminPage;
