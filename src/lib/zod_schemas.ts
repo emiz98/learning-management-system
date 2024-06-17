228e786e4c877507f0d127e56387bc18f4f986bc
Menura Adithya <64089619+emiz98@users.noreply.github.com>
2024-06-17 23:44:29 +0530

    added zod schemas

---
+++ src/lib/zod_schemas.ts
@@ -0,0 +1,41 @@
+import { z } from "zod";
+
+export const CourseSchema = z.object({
+  title: z.string().min(1, "Course name is required"),
+  description: z
+    .string()
+    .min(1, "Course description is required")
+    .max(300, "Too long description"),
+});
+
+export const UserSchema = z.object({
+  email: z.string().min(1, "Email is required").email("Invalid email"),
+  password: z
+    .string()
+    .min(1, "Password is required")
+    .min(8, "Password must have than 8 characters"),
+});
+
+export const EnrollmentSchema = z.object({
+  courseId: z.number(),
+});
+
+export const UserSignUpSchema = z
+  .object({
+    username: z.string().min(1, "Username is required").max(100),
+    email: z.string().min(1, "Email is required").email("Invalid email"),
+    password: z
+      .string()
+      .min(1, "Password is required")
+      .min(8, "Password must have than 8 characters"),
+    confirmPassword: z.string().min(1, "Password confirmation is required"),
+  })
+  .refine((data) => data.password === data.confirmPassword, {
+    path: ["confirmPassword"],
+    message: "Password do not match",
+  });
+
+export const UserUpdateSchema = z.object({
+  username: z.string().min(1, "Username is required").max(100),
+  email: z.string().min(1, "Email is required").email("Invalid email"),
+});
